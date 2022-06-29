ac4265e487d03aff40594ff939e107ae94720009
LawCR <alvacalderon99@gmail.com>
2022-06-29 14:55:39 -0500

    Estructura Basica de Socket Server

--- models/server.js
+++ models/server.js
@@ -1,8 +1,9 @@
-
+// Servidor de Express
 const express = require('express')
 const http = require('http')
 const socketio = require('socket.io')
 const path = require('path');
+const cors = require('cors')
 const Sockets = require('./sockets');
 
 class Server {
@@ -20,6 +21,9 @@ class Server {
     middlewares() {
         // Desplegar el directorio público
         this.app.use( express.static( path.resolve( __dirname, '../public') ))
+
+        // CORS
+        this.app.use( cors() )
     }
 
     configureSockets() {
